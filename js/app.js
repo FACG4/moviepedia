797428cbd8509708a9f17ff3dc759a5527c0da41
amusamhe <abdalsamad.y.m@gmail.com>
2018-03-27 12:58:15 +0200

    file structur

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function select() {
+
+}
+
+function addListener(element, action , cb) {
+
+}
+
+function fetch(url, cb) {
+
+}
+
+
+//render the section of dbmovie api
+
+
+
+
+
+// render the wikipeida section
+
+
+
+
+// (extra)render the book info section
+
